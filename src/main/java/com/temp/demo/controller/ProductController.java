8017862dcf7d692a1cba68489e5c514beda2ac01
teddy <teddy.pratama0123@gmail.com>
2026-09-01 20:44:42 +0700

    [UPDATE] Move get product to granted authority for reading

--- src/main/java/com/temp/demo/controller/ProductController.java
+++ src/main/java/com/temp/demo/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.temp.demo.dto.request.RequestCreateProductDTO;
 import com.temp.demo.dto.request.RequestDeleteProductDTO;
 import com.temp.demo.dto.request.RequestUpdateProductDTO;
 import com.temp.demo.dto.response.BasicResponse;
+import com.temp.demo.dto.response.ResponseCustomPaging;
 import com.temp.demo.dto.response.ResponseProductDTO;
 import com.temp.demo.service.ProductService;
 import com.temp.demo.util.Constants;
@@ -11,12 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(value = Constants.API_PATH + Constants.PRODUCT_PATH)
@@ -25,6 +29,20 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping(value = Constants.GET_PATH)
+    @PreAuthorize(value = "hasAuthority('PRODUCT_READ')")
+    public ResponseEntity<BasicResponse<ResponseCustomPaging<ResponseProductDTO>>> getProduct(@RequestParam(value = "name", required = false, defaultValue = "") String name,
+                                                                                              @RequestParam(value = "min_price", required = false) BigDecimal minPrice,
+                                                                                              @RequestParam(value = "max_price", required = false) BigDecimal maxPrice,
+                                                                                              @RequestParam(value = "sort_by", required = false, defaultValue = "price") String sortBy,
+                                                                                              @RequestParam(value = "sort_order", required = false, defaultValue = "asc") String sortOrder,
+                                                                                              @RequestParam(value = "page") int pageNumber,
+                                                                                              @RequestParam(value = "size") int pageSize) {
+        BasicResponse<ResponseCustomPaging<ResponseProductDTO>> response = new BasicResponse<>();
+        response.setSuccess(productService.getProduct(name, minPrice, maxPrice, sortBy, sortOrder, pageNumber, pageSize), "success");
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping(value = Constants.CREATE_PATH)
     @PreAuthorize(value = "hasAuthority('PRODUCT_CREATE')")
--- src/main/java/com/temp/demo/controller/PublicController.java
+++ src/main/java/com/temp/demo/controller/PublicController.java
@@ -6,29 +6,21 @@ import com.temp.demo.dto.request.RequestResetPasswordDTO;
 import com.temp.demo.dto.request.RequestStaffRegisterDTO;
 import com.temp.demo.dto.response.BasicResponse;
 import com.temp.demo.dto.response.ResponseAuthenticateDTO;
-import com.temp.demo.dto.response.ResponseCustomPaging;
-import com.temp.demo.dto.response.ResponseProductDTO;
-import com.temp.demo.entity.Staff;
 import com.temp.demo.service.DocumentService;
-import com.temp.demo.service.ProductService;
 import com.temp.demo.service.StaffService;
 import com.temp.demo.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
-import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(value = Constants.API_PATH + Constants.PUBLIC_PATH)
@@ -37,9 +29,6 @@ public class PublicController {
 
     @Autowired
     private StaffService staffService;
-
-    @Autowired
-    private ProductService productService;
 
     @Autowired
     private DocumentService documentService;
@@ -79,19 +68,6 @@ public class PublicController {
         BasicResponse<String> response = new BasicResponse<>();
         staffService.resetPassword(resetPasswordDTO);
         response.setSuccess("success", "success");
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping(value = Constants.PRODUCT_PATH + Constants.GET_PATH)
-    public ResponseEntity<BasicResponse<ResponseCustomPaging<ResponseProductDTO>>> getProduct(@RequestParam(value = "name", required = false, defaultValue = "") String name,
-                                                                                              @RequestParam(value = "min_price", required = false) BigDecimal minPrice,
-                                                                                              @RequestParam(value = "max_price", required = false) BigDecimal maxPrice,
-                                                                                              @RequestParam(value = "sort_by", required = false, defaultValue = "price") String sortBy,
-                                                                                              @RequestParam(value = "sort_order", required = false, defaultValue = "asc") String sortOrder,
-                                                                                              @RequestParam(value = "page") int pageNumber,
-                                                                                              @RequestParam(value = "size") int pageSize) {
-        BasicResponse<ResponseCustomPaging<ResponseProductDTO>> response = new BasicResponse<>();
-        response.setSuccess(productService.getProduct(name, minPrice, maxPrice, sortBy, sortOrder, pageNumber, pageSize), "success");
         return ResponseEntity.ok(response);
     }
 
