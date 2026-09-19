e09257d811b303452f8bfc26333139400a2136d6
teddy <teddy.pratama0123@gmail.com>
2026-09-19 16:11:46 +0700

    [UPDATE] remove ddl-auto update

--- src/main/java/com/temp/demo/entity/IdempotencyKey.java
+++ src/main/java/com/temp/demo/entity/IdempotencyKey.java
@@ -3,20 +3,14 @@ package com.temp.demo.entity;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 @Data
 @Entity
-@Table(name = "idempotency_keys",
-        uniqueConstraints = {
-                @UniqueConstraint(name = "uk_idempotency_key", columnNames = "idempotency_key")
-        }
-)
+@Table(name = "idempotency_keys")
 public class IdempotencyKey {
 
         @Id
@@ -24,15 +18,8 @@ public class IdempotencyKey {
         @GenericGenerator(name = "uuid-hibernate-generator", strategy = "org.hibernate.id.UUIDGenerator")
         private String id;
 
-        @Column(name = "idempotency_key", nullable = false, unique = true)
         private String idempotencyKey;
-
-        @Column(name = "hash_request", columnDefinition = "TEXT")
         private String hashRequest;
-
-        @Column(name = "response", columnDefinition = "TEXT")
         private String response;
-
-        @Column(name = "created_at", nullable = false)
         private Long createdAt;
 }
